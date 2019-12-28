722695a79f57ff465811504ea779ec5e0587156c
Lukas <Lukas.schoelch@gmail.com>
2019-12-28 14:54:18 +0100

    New folders for LocationSites, some creative tries

--- js/Websites.js
+++ js/Websites.js
@@ -3,9 +3,9 @@ function open_OxOst_location()
         location.href = "https://www.google.com/maps/place/Oxford+OST/@49.0093355,8.4239207,15z/data=!4m5!3m4!1s0x0:0x9b3b4995f5ef0d72!8m2!3d49.0093355!4d8.4239207";
     } 
 
-    function open_OxOst_daily()
+    function open_OxOst_site()
     {
-        location.href = "https://cdn.website.dish.co/media/34/ae/1745471/Oxford-East-Ost-Food-2-8-01.pdf";
+        location.href = "LocationSites/Oxfords/OxfordOst/OxOst.html";
     }
 
     function open_Mensa_site()
