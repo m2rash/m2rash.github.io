7ad4e69d56808663450be012d59eda6e71f05e59
Lukas <Lukas.schoelch@gmail.com>
2020-01-07 11:38:52 +0100

    Fixed card "alignment" in 2 col view
    Cards don't change order in 2 col view
    Changed name of "Mensa Adenauer-Ring"
    SortLocationListByScreenSize.js created but not implemented

--- js/SortLocationListByScreenSize.js
+++ js/SortLocationListByScreenSize.js
@@ -1,8 +1,14 @@
 
 function sortLocationList() {
-    var list, i, b,
+    var list, i, b, l,
     list = document.getElementById("LocationList");
     b = list.getElementsByTagName("LI");
+    l = b.length;
 
+    for (i = 0; i < (l - 1); i += 2) {
+        if (i % 2 != 0) {
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        }
+    }
 
 }
