847d0cf2096078e36a769d4f931ce8b7733a9773
Lukas <Lukas.schoelch@gmail.com>
2020-01-08 11:26:28 +0100

    Buttons on OxOst Page now have their function

--- js/Websites.js
+++ js/Websites.js
@@ -1,12 +1,42 @@
-function open_OxOst_location()
+
+/* Oxford Ost */
+
+
+function open_OxOst_site()
+{
+    location.href = "LocationSites/Oxfords/OxfordOst/OxOst.html";
+}
+
+function open_OxOst_extern()
+    {
+        location.href = "http://www.oxford-ost.de/";
+    }
+
+
+/*Buttons*/    
+
+function open_OxOst_Phone()
+    {
+        window.location.href = "tel:+4972166384455";
+    }
+
+function open_OxOst_Mail()
+    {
+        window.location.href = "mailto:reservierung@oxford-ost.de";
+    }
+
+function open_OxOst_Map()
     {
         location.href = "https://www.google.com/maps/place/Oxford+OST/@49.0093355,8.4239207,15z/data=!4m5!3m4!1s0x0:0x9b3b4995f5ef0d72!8m2!3d49.0093355!4d8.4239207";
     } 
 
-    function open_OxOst_site()
-    {
-        location.href = "LocationSites/Oxfords/OxfordOst/OxOst.html";
-    }
+
+
+
+/*************************************************************************/
+
+/* Mensa Adenauer */
+
     
     function open_Mensa_site()
     {
@@ -14,7 +44,4 @@ function open_OxOst_location()
     }
     
     
-    function open_OxOst_extern()
-    {
-        location.href = "http://www.oxford-ost.de/";
-    }
+    
